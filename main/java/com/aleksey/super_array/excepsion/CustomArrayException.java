8cb9452cc5ccbe7d4b05b6d088feb2d205cc2713
Aleksey27007 <alekseyvaruj@gmail.com>
2025-11-13 20:47:45 +0300

    added custom exception

---
+++ main/java/com/aleksey/super_array/excepsion/CustomArrayException.java
@@ -0,0 +1,8 @@
+package com.aleksey.super_array.excepsion;
+
+public class CustomArrayException extends Exception{
+
+    public CustomArrayException(String message) {
+        super(message);
+    }
+}
