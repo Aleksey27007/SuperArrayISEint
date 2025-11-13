90e3d187f277226e964b32f222f5259a3afb91c8
Aleksey27007 <alekseyvaruj@gmail.com>
2025-11-13 20:00:19 +0300

    added new sorting

--- main/java/com/aleksey/super_array/service/SortingSuperArrayService.java
+++ main/java/com/aleksey/super_array/service/SortingSuperArrayService.java
@@ -2,6 +2,6 @@ package com.aleksey.super_array.service;
 
 public interface SortingSuperArrayService {
     int[] quickSort(int[] array);
-    int[] mergeSort();
-    int[] insertionSort();
+    int[] mergeSort(int[] array, int left, int right);
+    int[] insertionSort(int[] array);
 }
--- main/java/com/aleksey/super_array/service/impl/SortingSuperArrayServiceImpl.java
+++ main/java/com/aleksey/super_array/service/impl/SortingSuperArrayServiceImpl.java
@@ -38,12 +38,62 @@ public class SortingSuperArrayServiceImpl implements SortingSuperArrayService {
     }
 
     @Override
-    public int[] mergeSort() { // O(n log n)
-        return new int[0];
+    public int[] mergeSort(int[] array, int left, int right) { // O(n log n)
+        if (left < right) {
+            // Находим середину массива
+            int middle = left + (right - left) / 2;
+
+            // Рекурсивно сортируем левую и правую половины
+            mergeSort(array, left, middle);
+            mergeSort(array, middle + 1, right);
+
+            // Сливаем отсортированные половины
+            merge(array, left, middle, right);
+        }
+        return array;
+    }
+
+    private static void merge(int[] array, int left, int middle, int right) {
+        // Создаем временные массивы для хранения данных
+        int size1 = middle - left + 1;
+        int size2 = right - middle;
+
+        int[] leftArray = new int[size1];
+        int[] rightArray = new int[size2];
+
+        // Копируем данные во временные массивы
+        System.arraycopy(array, left, leftArray, 0, size1);
+        for (int j = 0; j < size2; ++j) {
+            rightArray[j] = array[middle + 1 + j];
+        }
+        // Сливаем временные массивы обратно в основной массив array
+        int i = 0, j = 0, k = left;
+        while (i < size1 && j < size2) {
+            if (leftArray[i] <= rightArray[j]) {
+                array[k] = leftArray[i];
+                i++;
+            } else {
+                array[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+        // Копируем оставшиеся элементы leftArray[], если они есть
+        while (i < size1) {
+            array[k] = leftArray[i];
+            i++;
+            k++;
+        }
+        // Копируем оставшиеся элементы rightArray[], если они есть
+        while (j < size2) {
+            array[k] = rightArray[j];
+            j++;
+            k++;
+        }
     }
 
     @Override
-    public int[] insertionSort() { // O(n^2)
+    public int[] insertionSort(int[] array) { // O(n^2)
         return new int[0];
     }
 }
