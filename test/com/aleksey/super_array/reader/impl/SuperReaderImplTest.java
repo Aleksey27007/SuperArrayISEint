724ef8d8717c855adc6732a33ee1a3a279067124
Aleksey27007 <alekseyvaruj@gmail.com>
2025-11-18 00:36:26 +0300

    created test class

---
+++ test/com/aleksey/super_array/reader/impl/SuperReaderImplTest.java
@@ -0,0 +1,12 @@
+package com.aleksey.super_array.reader.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SuperReaderImplTest {
+
+    @Test
+    void superRead() {
+    }
+}
