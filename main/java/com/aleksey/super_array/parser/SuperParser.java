049feda856d524c80ff5af3767e9791bf4a8a8da
Aleksey27007 <alekseyvaruj@gmail.com>
2025-11-18 00:34:20 +0300

    renamed from SuperParser

--- main/java/com/aleksey/super_array/parser/SuperParser.java
+++
@@ -1,5 +0,0 @@
-package com.aleksey.super_array.parser;
-
-public interface SuperParser {
-    int[] parse(String text);
-}
---
+++ main/java/com/aleksey/super_array/parser/SuperParserInt.java
@@ -0,0 +1,7 @@
+package com.aleksey.super_array.parser;
+
+import java.util.ArrayList;
+
+public interface SuperParserInt {
+    int[] parse(ArrayList<String> stringArray);
+}
