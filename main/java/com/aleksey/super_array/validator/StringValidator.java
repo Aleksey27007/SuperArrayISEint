f81f89a5a8157ce893eb50c382b5d94799ae7482
Aleksey27007 <alekseyvaruj@gmail.com>
2025-11-17 21:30:14 +0300

    finished validator

---
+++ main/java/com/aleksey/super_array/validator/StringValidator.java
@@ -0,0 +1,11 @@
+package com.aleksey.super_array.validator;
+
+
+public class StringValidator {
+    private static final String GOOD_LINE_REGEX = "(\\d+[; .-]*)+";
+
+    public boolean isTheLineSuitable(String line) {
+        System.out.println(line.matches(GOOD_LINE_REGEX));
+        return line.matches(GOOD_LINE_REGEX);
+    }
+}
