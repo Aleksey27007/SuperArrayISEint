c28cd6aaa1003807c7d5e2ed996b49427eb22dca
Aleksey27007 <alekseyvaruj@gmail.com>
2025-11-17 21:33:19 +0300

    finished parser

--- main/java/com/aleksey/super_array/parser/impl/SuperParserImpl.java
+++ main/java/com/aleksey/super_array/parser/impl/SuperParserImpl.java
@@ -3,11 +3,11 @@ package com.aleksey.super_array.parser.impl;
 import com.aleksey.super_array.parser.SuperParser;
 
 public class SuperParserImpl implements SuperParser {
-    private static final String regex = "\\s*[;,.]\\s*";
+    private static final String REGEX = "\\s*[;,.]\\s*";
 
     @Override
     public int[] parse(String text) {
-        String [] substring = text.split(regex);
+        String [] substring = text.split(REGEX);
         int[] result = new int[substring.length];
 
         for (int i = 0; i < result.length; i++) {
