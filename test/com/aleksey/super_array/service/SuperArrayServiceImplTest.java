1056be120df1d00163bf7366d2a32ac9aa5f4852
Aleksey27007 <alekseyvaruj@gmail.com>
2025-11-18 20:28:56 +0300

    removed unused fields and imports

--- test/com/aleksey/super_array/service/SuperArrayServiceImplTest.java
+++ test/com/aleksey/super_array/service/SuperArrayServiceImplTest.java
@@ -2,51 +2,27 @@ package com.aleksey.super_array.service;
 
 import com.aleksey.super_array.entity.SuperArray;
 import com.aleksey.super_array.excepsion.CustomArrayException;
-import com.aleksey.super_array.service.SuperArrayService;
+
 import com.aleksey.super_array.service.impl.SuperArrayServiceImpl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.api.extension.RegisterExtension;
-import org.junit.jupiter.api.extension.TestWatcher;
 
 import java.util.Arrays;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class SuperArrayServiceImplTest {
 
-    static {
-        System.setProperty("log4j.configurationFile", "D:\\IntJava\\innowise\\super_array\\main\\resources\\log\\log4j2.xml");
-    }
-
     private static final Logger LOGGER = LogManager.getLogger();
-
-    @RegisterExtension
-    static TestWatcher loggingWatcher = new TestWatcher() {
-        @Override
-        public void testSuccessful(ExtensionContext context) {
-            String displayName = context.getDisplayName();
-            LOGGER.info("TEST PASSED: {}", displayName);
-        }
-
-        @Override
-        public void testFailed(ExtensionContext context, Throwable cause) {
-            String displayName = context.getDisplayName();
-            LOGGER.error("TEST FAILED: {} - {}", displayName, Optional.ofNullable(cause.getMessage()).orElse("no message"), cause);
-        }
-    };
 
     private SuperArray superArray;
     private SuperArray superArrayNull;
 
     private SuperArrayService service;
-    private SuperArrayService serviceForNull;
     private int min = -5;
     private int max = 12;
     private int avgNum = 2;
@@ -55,7 +31,6 @@ class SuperArrayServiceImplTest {
     private int negative = 2;
     private int exceptionIndex = 15;
     private int[] array;
-
 
 
     @BeforeEach
@@ -69,12 +44,9 @@ class SuperArrayServiceImplTest {
     }
 
     @Test
-    void shouldThrowException() throws CustomArrayException {
+    void shouldThrowException() {
 
-        Throwable thrown = assertThrows(
-                CustomArrayException.class,
-                () -> superArrayNull = SuperArray.builder().build()
-        );
+        Throwable thrown = assertThrows(CustomArrayException.class, () -> superArrayNull = SuperArray.builder().build());
 
         assertEquals("The array cannot be empty.", thrown.getMessage());
     }
@@ -98,12 +70,8 @@ class SuperArrayServiceImplTest {
         service.replaceElementOfArray(array, 5, 4);
         int[] actualArray = superArray.getArray();
 
-        Throwable thrown = assertThrows(
-                CustomArrayException.class,
-                () -> {
-                    service.replaceElementOfArray(array, exceptionIndex, 2000);
-                }
-        );
+        Throwable thrown = assertThrows(CustomArrayException.class,
+                () -> service.replaceElementOfArray(array, exceptionIndex, 2000));
 
         assertEquals(String.format("Index %d is out of bounds. Array length: %d", exceptionIndex, superArray.getArray().length), thrown.getMessage());
 
