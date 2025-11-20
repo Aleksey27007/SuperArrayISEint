36d287399cf5616f9a180befcd2fdaf9de52e4c4
Aleksey27007 <alekseyvaruj@gmail.com>
2025-11-20 16:57:20 +0300

    added throws

--- main/java/com/aleksey/super_array/reader/SuperReader.java
+++ main/java/com/aleksey/super_array/reader/SuperReader.java
@@ -1,7 +1,9 @@
 package com.aleksey.super_array.reader;
 
+import com.aleksey.super_array.excepsion.CustomArrayException;
+
 import java.util.List;
 
 public interface SuperReader {
-    List<String> superRead(String fileName);
+    List<String> superRead(String fileName) throws CustomArrayException;
 }
