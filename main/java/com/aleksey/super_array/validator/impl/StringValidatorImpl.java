617a55f5ae5e5693213e5db51c9dc06a985d240c
Aleksey27007 <alekseyvaruj@gmail.com>
2025-11-20 17:43:34 +0300

    added regex ,

--- main/java/com/aleksey/super_array/validator/impl/StringValidatorImpl.java
+++ main/java/com/aleksey/super_array/validator/impl/StringValidatorImpl.java
@@ -4,7 +4,7 @@ package com.aleksey.super_array.validator.impl;
 import com.aleksey.super_array.validator.StringValidator;
 
 public class StringValidatorImpl implements StringValidator {
-    private static final String GOOD_LINE_REGEX = "(\\d+[; .-]*)+";
+    private static final String GOOD_LINE_REGEX = "(\\d+[; ,.-]*)+";
     @Override
     public boolean isTheLineSuitable(String line) {
         return line.matches(GOOD_LINE_REGEX);
