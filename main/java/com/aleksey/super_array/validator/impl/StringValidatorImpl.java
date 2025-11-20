a2d289ce51de25c15ac85f228c714af7b9c6ee66
Aleksey27007 <alekseyvaruj@gmail.com>
2025-11-20 16:50:03 +0300

    added override method

--- main/java/com/aleksey/super_array/validator/impl/StringValidatorImpl.java
+++ main/java/com/aleksey/super_array/validator/impl/StringValidatorImpl.java
@@ -1,9 +1,11 @@
 package com.aleksey.super_array.validator.impl;
 
 
-public class StringValidatorImpl {
-    private static final String GOOD_LINE_REGEX = "(\\d+[; .-]*)+";
+import com.aleksey.super_array.validator.StringValidator;
 
+public class StringValidatorImpl implements StringValidator {
+    private static final String GOOD_LINE_REGEX = "(\\d+[; .-]*)+";
+    @Override
     public boolean isTheLineSuitable(String line) {
         return line.matches(GOOD_LINE_REGEX);
     }
