42ea7b6122662ae9851600db5faa6cab06b70af0
Aleksey27007 <alekseyvaruj@gmail.com>
2025-11-20 16:49:33 +0300

    added new constructors

--- main/java/com/aleksey/super_array/excepsion/CustomArrayException.java
+++ main/java/com/aleksey/super_array/excepsion/CustomArrayException.java
@@ -5,4 +5,12 @@ public class CustomArrayException extends Exception{
     public CustomArrayException(String message) {
         super(message);
     }
+
+    public CustomArrayException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CustomArrayException(Throwable cause) {
+        super(cause);
+    }
 }
