363291cd2061c3577bb4c9e6f39f239f14795b3b
Aleksey27007 <alekseyvaruj@gmail.com>
2025-11-20 17:32:21 +0300

    added List for text

--- test/com/aleksey/super_array/reader/impl/SuperReaderImplTest.java
+++ test/com/aleksey/super_array/reader/impl/SuperReaderImplTest.java
@@ -1,6 +1,7 @@
 package com.aleksey.super_array.reader.impl;
 
 import com.aleksey.super_array.excepsion.CustomArrayException;
+import com.aleksey.super_array.resources.string_lists_enum.StringsToTest;
 import com.aleksey.super_array.validator.impl.StringValidatorImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
@@ -20,19 +21,19 @@ class SuperReaderImplTest {
     @TempDir
     Path tempDirectory;
 
+    private List<String> actualList = Arrays.asList("1; 2; 3", "10; 5;; 4",
+            "1;3;3;4;1;6", "13;5; 6; 1; 14. 8", "11; 2; 1 ; 6; 1; 2; 9", "1; 2; 3",
+            "11; 2", "1; 2; 3", "11; 2");
+
     @Test
     void superReadKeepsOnlyValidatedLines() throws IOException, CustomArrayException {
         Path file = tempDirectory.resolve("numbers.txt");
-        Files.write(file, List.of(
-                "1; 2; 3",
-                "1; 2; x3; 6..5; 77 ",
-                "",
-                "11; 2"
-        ));
+
+        Files.write(file, StringsToTest.STRING_TO_TEST.getStringList());
 
         SuperReaderImpl reader = new SuperReaderImpl(tempDirectory, new StringValidatorImpl());
 
-        assertEquals(new ArrayList<>(Arrays.asList("1; 2; 3", "11; 2")), reader.superRead("numbers.txt"));
+        assertEquals(actualList, reader.superRead("numbers.txt"));
     }
 
     @Test
