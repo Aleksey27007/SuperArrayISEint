3133e127cad11f33cf70a7a0829ebc3a6ad48c7a
Aleksey27007 <alekseyvaruj@gmail.com>
2025-11-21 23:58:12 +0300

    rename regex

--- main/java/com/aleksey/super_array/validator/impl/StringValidatorImpl.java
+++ main/java/com/aleksey/super_array/validator/impl/StringValidatorImpl.java
@@ -4,9 +4,13 @@ package com.aleksey.super_array.validator.impl;
 import com.aleksey.super_array.validator.StringValidator;
 
 public class StringValidatorImpl implements StringValidator {
-    private static final String GOOD_LINE_REGEX = "(\\d+[; ,.-]*)+";
+    private static final String VALID_NUMERIC_LINE_REGEX = "\\s*\\d+(\\s*[; ,.]\\s*\\d+)*\\s*";
+    
     @Override
     public boolean isTheLineSuitable(String line) {
-        return line.matches(GOOD_LINE_REGEX);
+        if (line == null || line.isBlank()) {
+            return false;
+        }
+        return line.matches(VALID_NUMERIC_LINE_REGEX);
     }
 }
