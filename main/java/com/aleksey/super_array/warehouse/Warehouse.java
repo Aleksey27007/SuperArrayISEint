69ecfbbb4910e8123e33eb104ad8e54199faeab0
Aleksey27007 <alekseyvaruj@gmail.com>
2025-11-24 19:38:27 +0300

    added warehouse class

---
+++ main/java/com/aleksey/super_array/warehouse/Warehouse.java
@@ -0,0 +1,47 @@
+package com.aleksey.super_array.warehouse;
+
+import com.aleksey.super_array.entity.SuperArray;
+import com.aleksey.super_array.entity.SuperArrayStatistic;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class Warehouse {
+
+    private static final Logger logger = LogManager.getLogger();
+    private static Warehouse instance;
+    private final Map<Long, SuperArrayStatistic> statistics = new HashMap<>();
+
+    private Warehouse() {
+    }
+
+    public static Warehouse getInstance() {
+        if (instance == null) {
+            instance = new Warehouse();
+        }
+        return instance;
+    }
+
+    public void put(SuperArray superArray) {
+        update(superArray);
+    }
+
+    public void update(SuperArray superArray) {
+        if (superArray == null) {
+            return;
+        }
+        statistics.put(superArray.getId(), SuperArrayStatistic.from(superArray));
+    }
+
+    public Optional<SuperArrayStatistic> get(long id) {
+        return Optional.ofNullable(statistics.get(id));
+    }
+
+    public void remove(long id) {
+        statistics.remove(id);
+    }
+}
+
