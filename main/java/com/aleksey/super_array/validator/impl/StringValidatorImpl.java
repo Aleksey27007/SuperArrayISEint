d70078fc956c2848feee4b34cc3073fa35277df9
Aleksey27007 <alekseyvaruj@gmail.com>
2025-11-24 19:40:23 +0300

    added logger

--- main/java/com/aleksey/super_array/validator/impl/StringValidatorImpl.java
+++ main/java/com/aleksey/super_array/validator/impl/StringValidatorImpl.java
@@ -2,13 +2,17 @@ package com.aleksey.super_array.validator.impl;
 
 
 import com.aleksey.super_array.validator.StringValidator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class StringValidatorImpl implements StringValidator {
     private static final String VALID_NUMERIC_LINE_REGEX = "\\s*\\d+(\\s*[; ,.]\\s*\\d+)*\\s*";
+    private static final Logger logger = LogManager.getLogger();
     
     @Override
     public boolean isTheLineSuitable(String line) {
         if (line == null || line.isBlank()) {
+            logger.warn("This line is empty.");
             return false;
         }
         return line.matches(VALID_NUMERIC_LINE_REGEX);
