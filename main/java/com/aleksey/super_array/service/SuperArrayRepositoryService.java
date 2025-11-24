045c0c483b289443f448ff690351f04f46712c9c
Aleksey27007 <alekseyvaruj@gmail.com>
2025-11-24 22:17:24 +0300

    changed sign methods

---
+++ main/java/com/aleksey/super_array/service/SuperArrayRepositoryService.java
@@ -0,0 +1,19 @@
+package com.aleksey.super_array.service;
+
+import com.aleksey.super_array.entity.SuperArray;
+import com.aleksey.super_array.specification.SuperArraySpecification;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+public interface SuperArrayRepositoryService {
+    void add(SuperArray superArray);
+    void addAll(List<SuperArray> superArrays);
+    boolean removeById(long id);
+    boolean remove(SuperArray superArray);
+    Optional<SuperArray> findById(long id);
+    List<SuperArray> query(SuperArraySpecification specification);
+    List<SuperArray> sort(Comparator<SuperArray> comparator);
+    List<SuperArray> findAll();
+}
