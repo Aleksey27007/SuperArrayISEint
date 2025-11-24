870128569a566cf0f5d53591485a8568c830362d
Aleksey27007 <alekseyvaruj@gmail.com>
2025-11-24 22:11:57 +0300

    added observer,warehouse

--- main/java/com/aleksey/super_array/entity/SuperArray.java
+++ main/java/com/aleksey/super_array/entity/SuperArray.java
@@ -4,16 +4,17 @@ import com.aleksey.super_array.excepsion.CustomArrayException;
 
 import java.util.Arrays;
 
+import com.aleksey.super_array.observer.SuperArrayObserver;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class SuperArray {
-
-    private static final long MAX_LENGTH = 987654321098765432L;
+    private static final Logger logger = LogManager.getLogger();
     private final long id;
     private final int[] array;
-    private static final Logger logger = LogManager.getLogger();
+    private SuperArrayObserver observer;
+
 
     private SuperArray(SuperArrayBuilder superArrayBuilder) {
         this.id = superArrayBuilder.id;
@@ -54,16 +55,61 @@ public class SuperArray {
         return array;
     }
 
+    public int length() {
+        return array.length;
+    }
+
+    public void addObserver(SuperArrayObserver observer) {
+        this.observer = observer;
+    }
+
+    public void removeObserver() {
+        if (this.observer != null) {
+            this.observer = null;
+        }
+    }
+
+    public void setElement(int index, int value) throws CustomArrayException {
+        validateIndex(index);
+        array[index] = value;
+        notifyObserver();
+    }
+
+    private void validateIndex(int index) throws CustomArrayException {
+        if (index < 0 || index >= array.length) {
+            logger.log(Level.INFO, "Index out of bounds. SuperArray.validateIndex");
+            throw new CustomArrayException(String.format("Index %d is out of bounds. Array length: %d", index, array.length));
+        }
+    }
+
+    private void notifyObserver() {
+        if (observer != null) {
+            observer.update(this);
+        }
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof SuperArray that)) return false;
 
+        if (getId() != that.getId()) return false;
         return Arrays.equals(getArray(), that.getArray());
     }
 
     @Override
     public int hashCode() {
-        return Arrays.hashCode(getArray());
+        int result = Long.hashCode(getId());
+        result = 31 * result + Arrays.hashCode(getArray());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "SuperArray { id=%d," +
+                        " array=%s," +
+                        " size=%d",
+                id, Arrays.toString(array), array.length);
     }
 }
